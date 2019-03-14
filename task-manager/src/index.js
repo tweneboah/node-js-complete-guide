9b71fc07176577fae17601da7e9e405a6fcb1148
tweneboah <twentekghana@gmail.com>
2019-03-14 17:52:18 +0800

    uptime-app

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,28 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json())
 
+//Creating a user
+app.post('/users', (req, res)=>{
+    const user = new User(req.body)
+    user.save().then(()=> {
+         res.send(user)
+    }).catch((error) => {
+        res.status(400)
+      res.send(error)
+    })
+ }) 
+
+//Reading /fectching data 
+app.get('/users', (req, res) => {
+    User.find({}).then((users)=> {
+         res.send(users)
+    }).catch((e) => {
+
+    })
+})
+
+
+ //Creating a task
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
@@ -17,15 +39,7 @@ app.post('/tasks', (req, res) => {
     })
 })
 
-app.post('/users', (req, res)=>{
-   const user = new User(req.body)
-   user.save().then(()=> {
-        res.send(user)
-   }).catch((error) => {
-       res.status(400)
-     res.send(error)
-   })
-})
+
 
 
 app.listen(port, () => {
