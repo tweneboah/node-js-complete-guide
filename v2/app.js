07b2c9a2dff0d0bf9c219ce35614b07c1d532f22
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-04 09:59:33 +0800

    RESTful API/validation using Joi

--- v2/app.js
+++ v2/app.js
@@ -1,24 +1,154 @@
+const Joi = require('@hapi/joi')
+const express = require('express');
+
+const app = express();
+
+//MIDDLEWARE
+
+app.use(express.json())//This helps to pass the request as json and can be found in the req.body
+
+
+
+const courses = [
+ {id: 1, name: 'Node Js'},
+ {id: 2, name: 'React Js'},
+ {id: 3, name: 'Express Js'},
+ {id: 4, name: 'GraphQl Js'}
+]
+
+
+//====================================================
+//ROUTES
+//====================================================
+app.get('/', (req, res) => {
+ res.send('Hello Node Js')
+} )
+
+
+//Get all Courses
+
+app.get('/api/courses', (req, res) => {
+ res.send(courses)
+})
+
+
+//Get a single Course
+
+app.get('/api/courses/:courseId', (req, res) => {
+  const course = courses.find((course) => course.id === parseInt(req.params.courseId))
+  if(!course){
+   //return 404
+   res.status(404).send('The course with the given ID is not found')
+  }else {
+   res.send(course)
+  }
+})
+
+//POST  course
+app.post('/api/courses', (req, res) => {
+  //Validation using Joi
+  const schema = {
+    name: Joi.string().min(4).required()
+  }
+
+  const result = Joi.validate(req.body, schema)
+  console.log(result.error)
+  if(result.error){
+   //res.status(400).send(result.error);
+   res.status(400).send(result.error.details[0].message);
+   return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course)
+})
+
+
+
+//=========
+//UPDATE
+//=============
+
+app.put('/api/courses/:id', (req, res) => {
+   //Look up the course
+   const course = courses.find((course) => course.id === parseInt(req.params.courseId))
+   if(!course){
+    //If not existing, return 404
+    res.status(404).send('The course with the given ID is not found')
+   }
+  
+   //Validate
+    //Validation using Joi
+  const schema = {
+    name: Joi.string().min(4).required()
+  }
+  const result = Joi.validate(req.body, schema);
+
+
+   //If invalid, return 400 - Bad request
+   if(result.error){
+    //res.status(400).send(result.error);
+    res.status(400).send(result.error.details[0].message);
+    return;
+   }
+
+   //Update course
+
+   course.name = req.body.name
+   //Return the updated course
+   res.send(course)
+
+})
+
 
 //=================
-//MODULES
-//================
-
-//A module is an object that has many properties but we are concern with module.exports  console.log(modules)
-
-//In node any function we create becomes global so to avoid this we use what is call module. 
-
-//Module encapsulate all functions and make it private
-
-//Any file we created apart from the the main file becomes a module
-
-//To make a module/function/object public we pass it to module.expoorts
+//VALIDATION FUNCTION
+//===================
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(4).required()
+  }
+ return Joi.validate(req.body, schema);
+}
 
 
+
+//===========================
+//ROUTES PARAMETERS
 //==================
-//USING THE MODULE
-//=================
 
-const logger = require('./logger');
+//1. This is use when you want a detail about a particular object to perform any function on it
 
-logger.logMessage('WELCOME')
+
+//================
+//QUERY STRINGS
+//================
+//Query parameter is use to provide additional data to the backend services  and we use req.params to require values
+//We don't create a separate route for handling req.query
+
+//Sample route for query strings
+//localhost/post/2019/2?sortBy=name
+
+
+
+
+// ===================================================
+//ENVIRONMENT VARRIABLES
+//=====================================================
+
+//1. Setting up environment variable ->  export PORT=5000
+
+
+
+//SERVER
+
+app.listen(3000, () => {
+  console.log(`The server is runing`)
+})
+
+
 
--- v2/logger.js
+++
@@ -1,9 +0,0 @@
-
-//This module is use to log messages
-
-function logMessage(message){
- console.log(message)
-
-}
-
-module.exports.logMessage = logMessage
