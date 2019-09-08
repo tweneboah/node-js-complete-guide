543d9ba11d806f04bb9da663a54af4a05fde41e2
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-08 11:41:35 +0800

    Rendering message to the screen using mustache

---
+++ chat-app/v9/public/js/chat.js
@@ -0,0 +1,90 @@
+
+  const socket =  io();
+
+//ELEMEMTS
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = document.querySelector('button');
+const $locationButton = document.querySelector('#send-location');
+const $message = document.querySelector('#message')
+
+
+//DISPLAYING WELCOME MESSAGE
+
+//TEMPLATES 
+//  $message.insertAdjacentHTML('beforeend', html) this will add the text at the bottom of the div
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
+socket.on('message', (welcomeMessageFromEvent) => {
+   //Rendering this message to the screen
+   const html = Mustache.render(messageTemplate, {
+       message: welcomeMessageFromEvent
+   })
+  $message.insertAdjacentHTML('beforeend', html)
+   
+})
+
+
+
+  //CHECKING IF THE USER'S BROWSER SUPPORT LOCATION SHARING
+  $locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    //DISABLE BUTTON
+    $locationButton.setAttribute('disabled', 'disabled')
+
+    //Fetching location
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+
+      //This can also emit object as well
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+
+            //ENABLE LOCATION BUTTON
+            $locationButton.removeAttribute('disabled')
+    })
+})
+
+    //===============
+    //SENDING MESSAGE USING FORM
+
+    $messageForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        //DISABLE BUTTON ON FORM
+        $messageFormButton.setAttribute('disabled', 'disabled')
+
+    
+
+        const message = e.target.elements.message.value
+
+        socket.emit('sendMessage', message, (messageFromCallback) => {
+
+            console.log('Message ' ,messageFromCallback)
+            //ENABLE FORM BUTTON
+            $messageFormButton.removeAttribute('disabled')
+            
+            //CLEAR FORM INPUT
+            $messageFormInput.value = ''
+        
+            //FOCUS
+            $messageFormInput.focus();
+        })
+
+    })
+
+    //Sending messages from the form to all the
+    socket.on('message', (message) => {
+        console.log(message)
+    })
+
+    //receiving the event ftrom server
+    socket.on('myLocation', (coord) => {
+        console.log(coord)
+    })
+
---
+++ chat-app/v9/src/index.js
@@ -0,0 +1,57 @@
+  const path = require('path');
+  const express = require('express');
+  const socketio = require('socket.io');
+  const http = require('http');
+
+  const app = express();
+  const sever = http.createServer(app)
+
+  //CONFIG SOCKET
+  const io = socketio(sever)//Socket requires to pass the core http sever
+
+
+  const publicDirectoryPath = path.join(__dirname, '../public/')
+  app.use(express.static(publicDirectoryPath))
+
+
+  //EVENT ACKNOWLEDGEMT
+
+  //This is a message send to the only connected user that his/her message has been sent
+  //NOTE:
+  // When server emit an event and the client received, then the client will send acknowledgement to the server that the event was received and vice-versa
+
+  //The emit functions takes in 3 arguements; 
+  //1.The name of the event
+  //The data sending
+  //A function that sends acknowledgement
+
+  
+
+  io.on('connection', (socket) => {
+
+    //CREATING WELCOME MESSAGE VENT
+    socket.emit('message', 'You are welcome to our chat')
+
+
+    //RECEIVING EVENT FROM CLIENT
+    socket.on('sendLocation', (coords) => {
+      //sending back the response to all the connected clients
+      io.emit('myLocation',  `Location: https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+     
+    });
+
+    //For acknowledgement, we pass a second argument as a callback to socket.io
+    socket.on('sendMessage', (message, callback) => {
+      //Sending the message back to all users
+      io.emit('message', message)
+      callback('Was delivered') //Sending acknowledgemt. You can also pass data to the callback
+      
+    })
+  })
+
+
+
+//SERVER
+  sever.listen(3000, (req, res) => {
+  console.log('Server is runing right')
+  })
