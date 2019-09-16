25e21be4304a8cef9be9ffc599db8e8bbc5036a1
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-16 14:14:23 +0800

    removed

--- dev-v2-blog-post-data/models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-    title: String,
-    description: String
-});
-
-module.exports = mongoose.model('Post', postSchema);
