c5e12cd6a6de45e7462dca8306112e47b111fe66
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-16 13:53:39 +0800

    Remove duplicated directory

---
+++ chat-app/v12/src/index.js
@@ -0,0 +1,53 @@
+  const path = require('path');
+  const express = require('express');
+  const socketio = require('socket.io');
+  const http = require('http');
+  const {generateMessage} = require('./utils/messages')
+  const app = express();
+  const sever = http.createServer(app)
+  //CONFIG SOCKET
+  const io = socketio(sever)//Socket requires to pass the core http sever
+  const publicDirectoryPath = path.join(__dirname, '../public/')
+  app.use(express.static(publicDirectoryPath))
+
+
+  io.on('connection', (socket) => {
+    //CREATING WELCOME MESSAGE
+    socket.emit('welcomeMessage', generateMessage('Welcome'))
+
+    //EVENT WHEN A NEW USER JOIN THE CHAT
+    socket.broadcast.emit('alertMessageWhenUserJoin', 'A new user has joined');
+
+
+    //EVENT WHEN A USER IS DISCONNECTED
+    socket.on('disconnect', () => {
+      io.emit('disconnectedMessage', 'A User has left')
+    })
+
+
+    //RECEIVING EVENT FROM CLIENT
+    socket.on('shareMyLocation', (coords) => {
+      //sending back the response to all the connected clients
+  
+      io.emit('myLocation',  `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+     
+    });
+
+
+
+    //For acknowledgement, we pass a second argument as a callback to socket.io
+    socket.on('sendMessage', (message, callback) => {
+      //Sending the message back to all users
+      io.emit('messageFromForm', message)
+      callback('Was delivered') //Sending acknowledgemt. You can also pass data to the callback
+      
+    })
+  })
+
+
+
+
+//SERVER
+  sever.listen(3000, (req, res) => {
+  console.log('Server is runing right')
+  })
