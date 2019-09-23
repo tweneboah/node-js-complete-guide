eec8799ca15f40cf316cd0ffede3d1fca47a4ceb
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-23 22:49:22 +0800

    Showing and hidding navlink base on if a user is authenticated or not

---
+++ dev-blog-app/dev-blog-v14/app.js
@@ -0,0 +1,71 @@
+const methodOverride = require('method-override');
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+const User = require('./models/User');
+const postRoute = require('./routes/posts');
+const usersRoute = require('./routes/users');
+const commentsRoute = require('./routes/comments');
+const app = express();
+
+
+
+ 
+// DB CONNECTION
+  const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/Dev-Blog'
+
+  mongoose.connect( DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+    })
+    .then(() => console.log("DB Connected successfully"));
+
+    //MIDDLEWARE
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+//Serving static files
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+
+
+//Passport Configuration
+app.use(require('express-session')({
+    secret: 'Am an advance node developer',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use( new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.authUser = req.user
+    next();
+})
+
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+
+app.use('/', postRoute);
+app.use('/', usersRoute);
+app.use('/', commentsRoute);
+
+
+
+//SERVER
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`The Server is runing on port ${PORT}`)
+});
---
+++ dev-blog-app/dev-blog-v14/routes/posts.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const Post = require('../models/Post');
+const router = express.Router();
+const middleware = require('../middleware/index');
+
+
+
+//CREATE POST
+
+//1.GET the form
+router.get('/posts/new', middleware.isLogin, (req, res) => {
+    console.log('AUTH USER', req.user)
+    res.render('posts/createPost')
+})
+
+
+
+
+
+// CREATING POST LOGIC WITHOUT A USER
+
+router.post('/posts', middleware.isLogin, (req, res) => {
+     //Values from the form
+      const title = req.body.title;
+      const description = req.body.description;
+      //build the values as object
+      const newPost = {
+          title: title,
+          description: description
+      };
+
+      Post.create(newPost, (err, createdPost) => {
+          if(err){
+              console.log(err)
+          }else {
+              res.redirect('/posts');
+          }
+      })
+})
+
+//FETCHINF ALL POSTS
+router.get('/posts', middleware.isLogin, (req, res) => {
+    Post.find({}, (err, allPosts) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.render('posts/posts.ejs', {posts: allPosts})
+        }
+    })
+});
+
+
+
+//Show more route
+//Remember that at this point there is a comment created for this so we have to populate this comment and display it
+
+router.get('/posts/:id', (req, res) => {
+    Post.findById(req.params.id).populate('comments').exec((err, postWithComments) => {
+        if(err){
+            console.log(err)
+        }else{
+            
+            res.render('posts/show.ejs', {post: postWithComments, auth: req.user})
+        }
+    })
+});
+
+//Show more route Without comment
+
+// router.get('/posts/:id', (req, res) => {
+//     Post.findById(req.params.id, (err, foundpost) => {
+//         if(err){
+//             console.log(err)
+//         }else {
+//             res.render('posts/show.ejs', {post: foundpost})
+//         }
+//     })
+// });
+
+//DELETE
+router.delete('/posts/:id', middleware.isLogin, (req, res) => {
+    console.log('deleted')
+    Post.findByIdAndRemove(req.params.id, (err, foundPost) => {
+        if(err){
+            console.log(err)
+        }else {
+             res.redirect('/posts')
+        }
+    })
+});
+
+
+//EDIT POST
+
+//1. Create the edit form
+
+//2. Get the edit form and pass the post you want to edit to this form
+
+router.get('/posts/update/:id/new', (req, res) => {
+    //Find the post and pass it to this template
+    Post.findById(req.params.id, (err, foundPost) => {
+        if(err){
+            console.log(err)
+        }else {
+            res.render('posts/editPost.ejs', {post: foundPost})
+        }
+    })
+});
+
+//3.Update Logic
+
+// router.put('/posts/update/:id', (req, res) => {
+    
+//     //find the post you want to update and then retrieve the data from the form you want to update. Because of that we will build the data coming from the edit form and pass it as a second arguemnt to findByIdAndUpdate,
+//     let postToBeUpdate = {
+//         title: req.body.title,
+//         description: req.body.description
+//     };
+
+//     //Find the post you want to update
+//     Post.findByIdAndUpdate(req.params.id, postToBeUpdate, (err, updatedPost) => {
+//         if(err){
+//             console.log(err)
+//         }else {
+//             res.send(updatedPost)
+//         }
+    
+
+router.put('/posts/update/:id', (req, res) => {
+
+     //find the post you want to update and then retrieve the data from the form you want to update. Because of that we will build the data coming from the edit form and pass it as a second arguemnt to findByIdAndUpdate,
+
+           let postToBeUpdate = {
+        title: req.body.title,
+        description: req.body.description
+    };
+    //Find the post you want to update
+      Post.findByIdAndUpdate(req.params.id, postToBeUpdate, (err, updatedPost) => {
+          if(err){
+              console.log(err)
+          }else {
+            //res.send(updatedPost);
+            res.redirect(`/posts/${updatedPost.id}`)
+          }
+      })
+})
+module.exports = router;
