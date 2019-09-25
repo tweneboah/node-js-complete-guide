f9eb77b9ff76674fb6b6f2f43da9705b35b3c75d
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-25 08:31:39 +0800

    Project setup, ejs, server

---
+++ dev-blog-app/dev-blog-v1/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+
+//MIDDLEWARE
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+//Serving static files
+app.use(express.static(__dirname + "/public"));
+
+ 
+// DB CONNECTION
+  const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/Dev-Blog'
+
+  mongoose.connect( DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+    })
+    .then(() => console.log("DB Connected successfully.."));
+
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('App')
+})
+
+
+//SERVER
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`The Server is runing on port ${PORT}`)
+});
