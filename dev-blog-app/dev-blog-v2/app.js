72d5ae2e0850c4e984986d0e492a8285908ce63d
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-25 08:35:29 +0800

    Making post request

---
+++ dev-blog-app/dev-blog-v2/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+const app = express();
+
+
+
+ 
+// DB CONNECTION
+  const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/Dev-Blog'
+
+  mongoose.connect( DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+    })
+    .then(() => console.log("DB Connected successfully.."));
+
+    //MIDDLEWARE
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+//Serving static files
+app.use(express.static(__dirname + "/public"));
+
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('App')
+})
+
+
+//CREATE POST
+
+//1.GET the form
+app.get('/posts/new', (req, res) => {
+    res.render('posts/createPost')
+})
+
+//POST LOGIC
+app.post('/posts', (req, res) => {
+    //Values from the form
+    let title = req.body.title;
+    let description = req.body.description;
+    const newPost = {title: title, description: description};
+    Post.create(newPost, (err, newlyCreatedPost) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(newlyCreatedPost)
+        }
+    })
+
+})
+
+
+//SERVER
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`The Server is runing on port ${PORT}`)
+});
