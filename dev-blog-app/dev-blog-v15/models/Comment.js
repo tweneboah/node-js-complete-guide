ada035acc7ffb9e929e05765603d8e476cf3be63
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-27 08:29:48 +0800

    Authorisation, Attaching a user details to comment he/she created

--- dev-blog-app/dev-blog-v15/models/Comment.js
+++ dev-blog-app/dev-blog-v15/models/Comment.js
@@ -1,11 +1,15 @@
 const mongoose  = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-    //We will save the user who has logged in username and id to this comment
+    //We will save the user who has logged in username and id to this comment object
     author: {
-      id: String,
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User'
+      },
       username: String
     },
+    
     text: String
 });
 
